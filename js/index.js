c12d6c93ea902f613a2b187298d150786b674a30
lisseth2027-f <fabian.lopez2027@hilasal.superate.org>
2026-05-11 21:57:48 -0600

    Add this new branch

---
+++ js/index.js
@@ -0,0 +1,11 @@
+function continuar(){
+
+    let email = document.getElementById("email").value
+
+
+    if(email === ""){
+        alert("Por favor ingresa tu email o numero")
+    }else{
+        alert("bienvenido")
+    }
+}
